5acfcf0de2c9d5d176638ced293e342b94483c01
Denain <Denain@outlook.es>
2024-01-21 20:53:55 -0400

    estructura html

--- luego.php
+++ luego.php
@@ -6,3 +6,14 @@ echo 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum veniam volu
 
 echo "para habrir la terminal en VSCode hace falta presionar ctrl + ` (el simbolo que esta al lado del numero 1)"
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pagina</title>
+</head>
+<body>
+    <h1>mi pagina esta funcionando</h1>
+</body>
+</html>

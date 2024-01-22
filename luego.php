6b1187116356319134e35350fced2affb1aea991
Denain <Denain@outlook.es>
2024-01-21 20:49:53 -0400

    Explicacion

--- luego.php
+++ luego.php
@@ -3,4 +3,6 @@
 echo "megustaelpan"
 
 echo 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum veniam voluptatum hic, quia quod, porro dolorem dolores mollitia corrupti sint nulla fuga voluptate officia reprehenderit repellat rem a libero minima?'
+
+echo "para habrir la terminal en VSCode hace falta presionar ctrl + ` (el simbolo que esta al lado del numero 1)"
 ?>

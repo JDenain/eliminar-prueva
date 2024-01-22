2f8f9bd60401d2e6b1c224199f23a77764b16694
Denain <Denain@outlook.es>
2024-01-21 21:22:54 -0400

    funciona

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
 
-echo "megustaelpan"
+echo "megustaelpan";
 
-echo 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum veniam voluptatum hic, quia quod, porro dolorem dolores mollitia corrupti sint nulla fuga voluptate officia reprehenderit repellat rem a libero minima?'
+echo 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum veniam voluptatum hic, quia quod, porro dolorem dolores mollitia corrupti sint nulla fuga voluptate officia reprehenderit repellat rem a libero minima?';
 
-echo "para habrir la terminal en VSCode hace falta presionar ctrl + ` (el simbolo que esta al lado del numero 1)"
+echo "para habrir la terminal en VSCode hace falta presionar ctrl + ` (el simbolo que esta al lado del numero 1)";
 ?>
 <!DOCTYPE html>
 <html lang="en">

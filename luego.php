761c2af61389f2299e691072d04dd9fb03200b3a
Denain <Denain@outlook.es>
2024-01-21 20:44:52 -0400

    echos

---
+++ luego.php
@@ -0,0 +1,6 @@
+<?php
+
+echo "megustaelpan"
+
+echo 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum veniam voluptatum hic, quia quod, porro dolorem dolores mollitia corrupti sint nulla fuga voluptate officia reprehenderit repellat rem a libero minima?'
+?>
